83a0d59db846734c89605f7ea561ad6321bf6ca0
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-10-21 18:02:25 -0300

    Agregamos typeORM y docker-compose para la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,36 @@ import { EvaluacionRealizadaModule } from './evaluacion-realizada/evaluacion-rea
 import { DocenteModule } from './docente/docente.module';
 import { EvaluacionModule } from './evaluacion/evaluacion.module';
 import { CuestionarioModule } from './cuestionario/cuestionario.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [AlumnoModule, EvaluacionCuestionarioVersionadoModule, EvaluacionRealizadaModule, DocenteModule, EvaluacionModule, CuestionarioModule],
+  imports: [
+    AlumnoModule, 
+    EvaluacionCuestionarioVersionadoModule, 
+    EvaluacionRealizadaModule, 
+    DocenteModule, 
+    EvaluacionModule, 
+    CuestionarioModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      })
+    })
+  ]
 })
+
 export class AppModule {}
