e5c6a801fe9b6eefee67dc7ee162728c55d0f783
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-10-22 14:26:27 -0300

    Intento de hacer una query

--- src/alumno/alumno.controller.ts
+++ src/alumno/alumno.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AlumnoService } from './alumno.service'
 
-@Controller('alumno')
-export class AlumnoController {}
+@Controller('/alumno')
+export class AlumnoController {
+    constructor(private alumnoService: AlumnoService) {}
+
+    @Get()
+    getAllAlumnos() {
+        return 'this.alumnoService.findAll()'
+    }
+}
--- src/alumno/alumno.service.ts
+++ src/alumno/alumno.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Alumno } from 'src/alumno/alumno.entity';
+import { AppDataSource } from '../db/data-source';
 
 @Injectable()
-export class AlumnoService {}
+export class AlumnoService {
+
+    async findAll() {
+        const alumnos = await AppDataSource
+            .getRepository(Alumno)
+            .createQueryBuilder("user")
+            .getMany()
+
+        return alumnos
+    }
+}
