702fc5c5160d7252c8cadacd949b9fff5eed6d12
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-11-17 17:12:41 -0300

    Eliminamos cuestionario del modelo de datos

--- src/docente/docente.service.ts
+++ src/docente/docente.service.ts
@@ -8,46 +8,45 @@ export class DocenteService {
   constructor(
     @InjectRepository(Docente)
     private readonly docenteRepository: Repository<Docente>,
-  ) {}  
-  
+  ) {}
+
   async findAll() {
-      const docentes = await this.docenteRepository.find({
-        select: ['id', 'nombre', 'apellido', 'email', 'dni'],
+    const docentes = await this.docenteRepository.find({
+      select: ['id', 'nombre', 'apellido', 'email', 'dni'],
     });
 
     return docentes;
-    }
+  }
 
-    async findById(id: number) {
-      const docente = await this.docenteRepository.findOne({
-        where: { id },
-        select: ['id', 'nombre', 'apellido', 'email', 'dni'],
-      });
-      return docente;
-    }
+  async findById(id: number) {
+    const docente = await this.docenteRepository.findOne({
+      where: { id },
+      select: ['id', 'nombre', 'apellido', 'email', 'dni'],
+    });
+    return docente;
+  }
 
-    async findByDni(dni: number) {
-        const docente = await this.docenteRepository.findOne({
-            where: { dni },
-            select: ['id', 'nombre', 'apellido', 'email', 'dni'],
-        });
-        return docente;
-    }
+  async findByDni(dni: number) {
+    const docente = await this.docenteRepository.findOne({
+      where: { dni },
+      select: ['id', 'nombre', 'apellido', 'email', 'dni'],
+    });
+    return docente;
+  }
 
-    async create(docenteData: Docente) {
-      const nuevoDocente = this.docenteRepository.create(docenteData);
-      return await this.docenteRepository.save(nuevoDocente);
-    }
-  
-    async delete(id: number) {
-        const result = await this.docenteRepository.delete(id);
-        return result
-      }
+  async create(docenteData: Docente) {
+    const nuevoDocente = this.docenteRepository.create(docenteData);
+    return await this.docenteRepository.save(nuevoDocente);
+  }
 
-      async modifyById(id: number, docenteData: Docente) {
-        const docente = await this.docenteRepository.findOne({where: {id}})
-        Object.assign(docente, docenteData)
-        this.docenteRepository.save(docente)
-      }
-      
+  async delete(id: number) {
+    const result = await this.docenteRepository.delete(id);
+    return result;
+  }
+
+  async modifyById(id: number, docenteData: Docente) {
+    const docente = await this.docenteRepository.findOne({ where: { id } });
+    Object.assign(docente, docenteData);
+    this.docenteRepository.save(docente);
+  }
 }
