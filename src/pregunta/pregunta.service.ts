75fc347281561b16d09c92a67c06b46ba54ba84b
AdryPika <adriana.picallo@estudiantes.unahur.edu.ar>
2024-11-18 18:15:48 -0300

    Se agrego controller y service de pregunta

--- src/pregunta/pregunta.service.ts
+++ src/pregunta/pregunta.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Pregunta } from './pregunta.entity';
 
 @Injectable()
-export class PreguntaService {}
+export class PreguntaService {
+    constructor(
+        @InjectRepository(Pregunta)
+        private readonly preguntaRepository: Repository<Pregunta>,
+    ){}
+
+
+    async findAll() {
+        const preguntas = await this.preguntaRepository.find({
+         select: ['id', 'pregunta', 'puntaje'],
+        });
+        return preguntas
+    }
+
+    async findById(id: number) {
+        const pregunta = await this.preguntaRepository.findOne({
+          where: { id },
+          select: ['id', 'pregunta', 'puntaje'],
+        });
+        return pregunta;
+    }
+
+    async create(preguntaData: Pregunta) {
+        const nuevaPregunta = this.preguntaRepository.create(preguntaData);
+        return await this.preguntaRepository.save(nuevaPregunta);
+    }
+    
+    async delete(id: number) {
+        await this.preguntaRepository.delete(id);
+        
+    }
+
+    async modifyById(id: number, preguntaData: Pregunta) {
+        const pregunta = await this.preguntaRepository.findOne({ where: { id } });
+        Object.assign(pregunta, preguntaData);
+        this.preguntaRepository.save(pregunta);
+    }
+
+
+}
