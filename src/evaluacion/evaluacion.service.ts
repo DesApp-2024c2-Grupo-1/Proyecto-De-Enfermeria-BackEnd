74ef281a42211e25f2a20589de9d21ce1d402bb0
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2024-11-20 16:49:15 -0300

    Se agrego el super endpoint para crear una evaluacion con sus preguntas

--- src/evaluacion/evaluacion.service.ts
+++ src/evaluacion/evaluacion.service.ts
@@ -1,14 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { Evaluacion } from './evaluacion.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { Pregunta } from 'src/pregunta/pregunta.entity';
+import { Docente } from 'src/docente/docente.entity';
 
 @Injectable()
 export class EvaluacionService {
   constructor(
     @InjectRepository(Evaluacion)
     private readonly evaluacionRepository: Repository<Evaluacion>,
+    @InjectRepository(Pregunta)
+    private readonly preguntaRepository: Repository<Pregunta>
   ) {}
+
+    async createEvaluacionYPreguntas(evaluacionyPreguntasData: { titulo: string; exigencia: string; docente:DeepPartial<Docente>; preguntas: { pregunta: string; puntaje: number }[] }) {
+      const { titulo, exigencia, docente, preguntas } = evaluacionyPreguntasData
+
+      //evaluacion
+      const nuevaEvaluacion = this.evaluacionRepository.create({titulo, exigencia, docente});
+      const evaluacionGuardada = await this.evaluacionRepository.save(nuevaEvaluacion);
+
+      //preguntas
+      const preguntasGuardadas = await Promise.all(
+        preguntas.map(async (preguntaData) => {
+          const nuevaPregunta = this.preguntaRepository.create({
+            pregunta: preguntaData.pregunta,
+            puntaje: preguntaData.puntaje,
+            evaluacion: evaluacionGuardada, // Relacionar la pregunta con la evaluación
+          });
+          return await this.preguntaRepository.save(nuevaPregunta);
+        }),
+      );
+      
+        return {
+          evaluacion: evaluacionGuardada,
+          preguntas: preguntasGuardadas,
+        };
+    }
 
     async findAll() {
       const evaluaciones = await this.evaluacionRepository.find({
@@ -33,6 +62,8 @@ export class EvaluacionService {
         return await this.evaluacionRepository.save(nuevoEvaluacion);
         }
   
+      /*
+
       async delete(id: number) {
         const salida = await this.evaluacionRepository.delete(id);
       return salida
@@ -45,5 +76,6 @@ export class EvaluacionService {
           
       }
 
+      */
   }
 
