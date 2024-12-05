bb48811e541dab525c2ff7f3ba189dbea33dc1db
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2024-12-05 15:49:33 -0300

    Se arreglo el dto de put Docente

--- src/docente/DocenteDTO/putDocente.dto.ts
+++ src/docente/DocenteDTO/putDocente.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsInt, IsOptional, IsString, Max, Min, MinLength } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 import { EvaluacionRealizada } from "src/evaluacion-realizada/evaluacion-realizada.entity";
 import { Evaluacion } from "src/evaluacion/evaluacion.entity";
 
@@ -7,26 +7,20 @@ export class PutDocenteRequestDTO {
 @IsString()
 @MinLength(3, { message: "El nombre debe tener al menos 3 letras" })
 @IsOptional()
+@IsNotEmpty({message: "El nombre no puede estar vacio"})
 nombre: string;
 
 @IsString({ message: "El apellido debe ser una cadena de caracteres" })
 @MinLength(3, { message: "El apellido debe tener al menos 3 letras" })
 @IsOptional()
+@IsNotEmpty({message: "El apellido no puede estar vacio"})
 apellido?: string;
 
-@IsEmail({}, { message: "El email debe ser válido" })
-@IsOptional()
+
 email?: string;
 
-@IsInt({ message: "El DNI debe ser un número" })
-@Min(1000000, { message: "El DNI debe ser un número de al menos 7 caracteres" })
-@Max(99999999, { message: "El DNI debe ser un número de hasta 8 caracteres" })
-@IsOptional() 
 dni?: number;
 
-@IsString({ message: "La contraseña debe ser una cadena de caracteres" })
-@MinLength(8, { message: "La contraseña debe tener al menos 8 caracteres" })
-@IsOptional()  
 password?: string;
 
 evaluacion?: Evaluacion[];
