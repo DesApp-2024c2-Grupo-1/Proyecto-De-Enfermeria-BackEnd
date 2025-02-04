77835b526c138a2c2c2722dfea4a90c07e8c431a
Pablen Israelsky <pabloisraelsky@gmail.com>
2025-02-04 14:52:56 -0300

    Modificamos la configuracion de puertos y el dockerfile

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import seedDatabase from './seeders/seed-data';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3001',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   })
