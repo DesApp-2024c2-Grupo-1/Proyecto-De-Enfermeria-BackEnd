37aa26ba843e2173d454fc874ff2585a3bc8ad1f
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2025-04-20 19:31:38 -0300

    Se arreglo el service y la entidad de evaluacion realizada con la normalizacion de lugar evaluacion

--- src/lugar-evaluacion/lugar-evaluacion.service.ts
+++ src/lugar-evaluacion/lugar-evaluacion.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { LugarEvaluacion } from './lugar-evaluacion.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
