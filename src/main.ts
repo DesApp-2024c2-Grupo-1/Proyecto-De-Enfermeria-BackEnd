5ceb95be5d77f2353aabfccb3094d6ab198eae4c
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2025-07-23 09:37:09 -0300

    Se agrego en la configuracion lo necesario para utilizar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import seedDatabase from './seeders/seed-data';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,18 @@ async function bootstrap() {
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   })
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('API Registros de Evaluacion de Enfermeria')
+    .setDescription('Documentacion de la API de registros de evaluacion')
+    //.addBearerAuth() //Si despues incluimos JWT deberiamos poner esto porque le daria el soporte visual para el token
+    .setVersion('1.0')
+    .build()
+
+    const document = SwaggerModule.createDocument(app, configSwagger)
+    SwaggerModule.setup('api', app, document)
+
+
   await seedDatabase();
   await app.listen(process.env.PORT ?? 3000);
 }
